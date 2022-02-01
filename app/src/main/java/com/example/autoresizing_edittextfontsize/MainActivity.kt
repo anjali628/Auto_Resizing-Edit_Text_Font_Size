30ede7794a3561551a1a60a840338d170ba5dff4
Anjali Bindal <anjalibindal628@gmail.com>
2022-02-01 18:15:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/autoresizing_edittextfontsize/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.autoresizing_edittextfontsize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText=findViewById(R.id.edit_text)
+        textView=findViewById(R.id.text_view)
+
+        editText.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textView.setText(p0.toString())
+                var size=textView.textSize
+                size=size/3
+                editText.textSize=size
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+
+    }
+}
